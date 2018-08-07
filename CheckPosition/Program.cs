bca27350ef7abef11cdc033ae309ff59cbe96c7a
shubhamsoni6050 <40332828+shubhamsoni6050@users.noreply.github.com>
2018-08-07 14:25:16 +0530

    initial commit

---
+++ CheckPosition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int marks;
+            Console.WriteLine("Enter your marks");
+            marks = int.Parse(Console.ReadLine());
+            if (marks>=80)
+            {
+                Console.WriteLine("First Position");
+            }
+            else if (marks>=70&marks<80)
+            {
+                Console.WriteLine("Second position");
+            }
+            else if (marks >= 40 & marks < 70)
+            {
+                Console.WriteLine("Third position");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            Console.ReadLine();
+        }
+    }
+}
